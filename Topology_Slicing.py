2d61756c6f0b11df806ff024647da299479dd022
timothy marchant <tim.marchant@outlook.com>
2026-04-05 00:41:21 -0400

    Fixed port issues with switches.  I need to figure out openflow.

--- Topology_Slicing.py
+++ Topology_Slicing.py
@@ -36,15 +36,19 @@ def CameraTopology():
         CameraAccessPoint4=net.addAccessPoint('Camera4AP', ssid='ssid-Camera4AP', channel='1', position='1010,1005,0')
         #call remote so SDN controller works.  This example uses a dumb L2 switch for establishing a baseline.
         c1=net.addController('c1',controller=RemoteController,ip=ryu_ip,port=ryu_port)
+
         s1=net.addSwitch('s1')
         s2=net.addSwitch('s2')
         s3=net.addSwitch('s3')
+        s4=net.addSwitch('s4')
+        s5=net.addSwitch('s5')
+        s6=net.addSwitch('s6')
 
         ###Use channel 2 overlaps with channel 1.
         EmergencyAP1=net.addAccessPoint('Emergency1AP', ssid='ssid-Emergency1AP', channel='2', position='500,5,0')
-        EmergencyAP2=net.addAccessPoint('Emergency2AP', ssid='ssid-Emergency2AP', channel='2', position='1500,5,0')
-        EmergencyAP3=net.addAccessPoint('Emergency3AP', ssid='ssid-Emergency3AP', channel='2', position='2500,5,0')
-        EmergencyAP4=net.addAccessPoint('Emergency4AP', ssid='ssid-Emergency4AP', channel='2', position='3200,5,0')
+        EmergencyAP2=net.addAccessPoint('Emergency2AP', ssid='ssid-Emergency2AP', channel='2', position='1000,500,0')
+        EmergencyAP3=net.addAccessPoint('Emergency3AP', ssid='ssid-Emergency3AP', channel='2', position='0,500,0')
+        EmergencyAP4=net.addAccessPoint('Emergency4AP', ssid='ssid-Emergency4AP', channel='2', position='500,1000,0')
         #Add wire hosts
         EmergencyCenter=net.addHost('Emerctr') #name is character limited
         Middlebox1=net.addHost('Mid1')
@@ -54,32 +58,31 @@ def CameraTopology():
 
         net.configureNodes()
         #temp.  Should be removed later.
-        net.addLink(Cameras1,CameraAccessPoint1,bw=10)
-        net.addLink(Cameras2,CameraAccessPoint2,bw=10)
-        net.addLink(Cameras3,CameraAccessPoint3,bw=10)
-        net.addLink(Cameras4,CameraAccessPoint4,bw=10)
 
-        
-
-        
         
         #Connections from hosts to switches
-        net.addLink(Middlebox1,s1,bw=50)
-        net.addLink(Middlebox2,s2,bw=50)
+        net.addLink(Middlebox1,s1,port1=1,port2=1,bw=50)
+        net.addLink(Middlebox2,s2,port1=1,port2=1,bw=50)
 
         net.addLink(CameraAccessPoint1,s1,bw=20) #Connect to S1.  Connects to box 1
         net.addLink(CameraAccessPoint2,s1,bw=20) #Connect to S1.  Connects to box 1
         net.addLink(CameraAccessPoint3,s2,bw=20) #Connect to S2.  Connects to box 2
         net.addLink(CameraAccessPoint4,s2,bw=20) #Connect to S2.  Connects to box 2
 
-        net.addLink(EmergencyCenter,s3,bw=100) #High bandwidth
+        net.addLink(EmergencyCenter,s5,port2=4,bw=100) #High bandwidth
+        net.addLink(s1,s5,port2=1,bw=50)
+        net.addLink(s2,s5,port2=2,bw=50)
+        net.addLink(s1,s3,port2=1,bw=100) #cannot make switch from Box 1 to S3 without causing problems.
+        net.addLink(s2,s3,port2=2,bw=100) #cannot make switch from Box 2 to S3 without causing problems.
+        net.addLink(s3,s4,port2=1,bw=100)
+        net.addLink(s3,s5,port2=3,bw=100)
 
-        net.addLink(s1,s3,bw=100) #cannot make switch from Box 1 to S3 without causing problems.
-        net.addLink(s2,s3,bw=100) #cannot make switch from Box 2 to S3 without causing problems.
+        net.addLink(Datacenter,s4,port2=2,bw=100) #Data storage platform.
 
-        net.addLink(Datacenter,s3,bw=100) #Data storage platform.
-
-
+        net.addLink(Cameras1,CameraAccessPoint1,bw=10)
+        net.addLink(Cameras2,CameraAccessPoint2,bw=10)
+        net.addLink(Cameras3,CameraAccessPoint3,bw=10)
+        net.addLink(Cameras4,CameraAccessPoint4,bw=10)
 
 
         net.plotGraph(max_x=1050,max_y=1050)
