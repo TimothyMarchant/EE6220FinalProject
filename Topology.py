3e9078f1de2a2d3027eec98c15e8a8559d37489b
timothy marchant <tim.marchant@outlook.com>
2026-04-04 23:15:43 -0400

    Changed camera positions to be more realistic (now a square formation).  Also fixed bandwidth stuff.  Topology.py uses L2switch.py.  Honestly I hate mininet, really a terrible platform.  TI has better documentation for their HAL (btw the documentation is in the .h files for HAL).

--- Topology.py
+++ Topology.py
@@ -21,19 +21,19 @@ ryu_port = 6653
 
 def CameraTopology():
         global net
-    #Camera topology
+         #High bandwidth#Camera topology
         info("Creating Nodes\n")
-        
+        #define cameras
         Cameras1 = net.addStation('Cameras1', mac='00:00:00:00:00:01', position='1,0,0')
         Cameras2 = net.addStation('Cameras2', mac='00:00:00:00:00:02', position='1000,0,0')
-        Cameras3 = net.addStation('Cameras3', mac='00:00:00:00:00:03', position='2000,0,0')
-        Cameras4 = net.addStation('Cameras4', mac='00:00:00:00:00:04', position='3000,0,0')
+        Cameras3 = net.addStation('Cameras3', mac='00:00:00:00:00:03', position='0,1000,0')
+        Cameras4 = net.addStation('Cameras4', mac='00:00:00:00:00:04', position='1000,1000,0')
         ###Use channel 1.  They do not overlap.
         CameraAccessPoint1=net.addAccessPoint('Camera1AP', ssid='ssid-Camera1AP', channel='1', position='1,5,0')
-        CameraAccessPoint2=net.addAccessPoint('Camera2AP', ssid='ssid-Camera2AP', channel='1', position='1000,5,0')
-        CameraAccessPoint3=net.addAccessPoint('Camera3AP', ssid='ssid-Camera3AP', channel='1', position='2000,5,0')
-        CameraAccessPoint4=net.addAccessPoint('Camera4AP', ssid='ssid-Camera4AP', channel='1', position='3000,5,0')
-
+        CameraAccessPoint2=net.addAccessPoint('Camera2AP', ssid='ssid-Camera2AP', channel='1', position='1010,5,0')
+        CameraAccessPoint3=net.addAccessPoint('Camera3AP', ssid='ssid-Camera3AP', channel='1', position='10,5,0')
+        CameraAccessPoint4=net.addAccessPoint('Camera4AP', ssid='ssid-Camera4AP', channel='1', position='1010,1005,0')
+        #call remote so SDN controller works.  This example uses a dumb L2 switch for establishing a baseline.
         c1=net.addController('c1',controller=RemoteController,ip=ryu_ip,port=ryu_port)
         s1=net.addSwitch('s1')
         s2=net.addSwitch('s2')
@@ -44,7 +44,7 @@ def CameraTopology():
         EmergencyAP2=net.addAccessPoint('Emergency2AP', ssid='ssid-Emergency2AP', channel='2', position='1500,5,0')
         EmergencyAP3=net.addAccessPoint('Emergency3AP', ssid='ssid-Emergency3AP', channel='2', position='2500,5,0')
         EmergencyAP4=net.addAccessPoint('Emergency4AP', ssid='ssid-Emergency4AP', channel='2', position='3200,5,0')
-
+        #Add wire hosts
         EmergencyCenter=net.addHost('Emerctr') #name is character limited
         Middlebox1=net.addHost('Mid1')
         Middlebox2=net.addHost('Mid2')
@@ -52,7 +52,7 @@ def CameraTopology():
         #net.setPropagationModel(model='logDistance', exp=3)
 
         net.configureNodes()
-        
+        #temp.  Should be removed later.
         net.addLink(Cameras1,CameraAccessPoint1,bw=10)
         net.addLink(Cameras2,CameraAccessPoint2,bw=10)
         net.addLink(Cameras3,CameraAccessPoint3,bw=10)
@@ -62,27 +62,30 @@ def CameraTopology():
 
         
         
-
+        #Connections from hosts to switches
         net.addLink(Middlebox1,s1,bw=50)
         net.addLink(Middlebox2,s2,bw=50)
-        net.addLink(CameraAccessPoint1,s1,bw=20)
-        net.addLink(CameraAccessPoint2,s1,bw=20)
-        net.addLink(CameraAccessPoint3,s2,bw=20)
-        net.addLink(CameraAccessPoint4,s2,bw=20)
-        net.addLink(EmergencyCenter,s3,bw=100)
-        net.addLink(s1,s3,bw=100)
-        net.addLink(s2,s3,bw=100)
-        net.addLink(Datacenter,s3,bw=100)
 
-      #  net.addLink(EmergencyCenter,Middlebox1)
-      #  net.addLink(EmergencyCenter,Middlebox2)
+        net.addLink(CameraAccessPoint1,s1,bw=20) #Connect to S1.  Connects to box 1
+        net.addLink(CameraAccessPoint2,s1,bw=20) #Connect to S1.  Connects to box 1
+        net.addLink(CameraAccessPoint3,s2,bw=20) #Connect to S2.  Connects to box 2
+        net.addLink(CameraAccessPoint4,s2,bw=20) #Connect to S2.  Connects to box 2
+
+        net.addLink(EmergencyCenter,s3,bw=100) #High bandwidth
+
+        net.addLink(s1,s3,bw=100) #cannot make switch from Box 1 to S3 without causing problems.
+        net.addLink(s2,s3,bw=100) #cannot make switch from Box 2 to S3 without causing problems.
+
+        net.addLink(Datacenter,s3,bw=100) #Data storage platform.
 
 
-        net.plotGraph(max_x=3250,max_y=2050)
+
+
+        net.plotGraph(max_x=1050,max_y=1050)
 
         net.build()
         c1.start()
-      #  c2.start()
+
         CameraAccessPoint1.start([c1])
         CameraAccessPoint2.start([c1])
         CameraAccessPoint3.start([c1])
@@ -91,16 +94,9 @@ def CameraTopology():
         EmergencyAP2.start([c1])
         EmergencyAP3.start([c1])
         EmergencyAP4.start([c1])
-      #  Middlebox1.start([c1])
-      #  Middlebox2.start([c1])
-      #  EmergencyCenter.start([c1])
 
-def Middleboxes():
-        print("run")
-def EmergencyCenter():
-        print("run")
-def SetupSDN():
-        print("")
+
+
 
 setLogLevel('info')
 
