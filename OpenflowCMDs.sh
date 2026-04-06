a8c0ff3a50a6cfab3b415d801c49533cf7bb8b91
timothy marchant <tim.marchant@outlook.com>
2026-04-05 21:46:33 -0400

    added openflow commands bash script.  fixed Camera3AP position.  Need to figure out why iperf breaks with the second middlebox.

---
+++ OpenflowCMDs.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#s1,s2,s3,s4,s5
+#define IP addresses
+Camera1IPRange=10.0.0.0/24
+Camera2IPRange=10.0.1.0/24
+MiddleboxIPRange=10.0.4.0/24
+EmergencyCenterIP=200.0.0.0
+EmergencyVehiclesIPRange=12.0.0.0/16
+#port towards middlebox
+MiddleboxPort=1
+
+
+ovs-ofctl add-flow s1 priority=500,ip,nw_src=$Camera1IPRange,actions=output:$MiddleboxPort
+ovs-ofctl add-flow s1 priority=500,icmp,nw_src=$Camera1IPRange,actions=output:$MiddleboxPort
+
+ovs-ofctl add-flow s1 priority=500,in_port=1,ip,nw_dst=$Camera1IPRange,actions=normal
+ovs-ofctl add-flow s1 priority=500,in_port=1,icmp,nw_dst=$Camera1IPRange,actions=normal
+
+ovs-ofctl add-flow s1 priority=500,ip,nw_src=10.0.0.1,actions=output:$MiddleboxPort
+ovs-ofctl add-flow s1 priority=500,in_port=1,ip,nw_dst=10.0.0.1,actions=normal
+ovs-ofctl add-flow s1 priority=500,ip,nw_src=10.0.0.2,actions=output:$MiddleboxPort
+ovs-ofctl add-flow s1 priority=500,in_port=1,ip,nw_dst=10.0.0.2,actions=normal
+
+
+
+
+ovs-ofctl add-flow Camera1AP actions=normal
+ovs-ofctl add-flow Camera2AP actions=normal
+
+#s2 defintions
+ovs-ofctl add-flow s2 priority=500,ip,nw_src=$Camera2IPRange,actions=output:$MiddleboxPort
+ovs-ofctl add-flow s2 priority=500,icmp,nw_src=$Camera2IPRange,actions=output:$MiddleboxPort
+
+ovs-ofctl add-flow s2 priority=500,in_port=1,ip,nw_dst=$Camera2IPRange,actions=normal
+ovs-ofctl add-flow s2 priority=500,in_port=1,icmp,nw_dst=$Camera2IPRange,actions=normal
+
+ovs-ofctl add-flow s2 priority=500,ip,nw_src=10.0.1.1,actions=output:$MiddleboxPort
+ovs-ofctl add-flow s2 priority=500,in_port=1,ip,nw_dst=10.0.1.1,actions=normal
+ovs-ofctl add-flow s2 priority=500,ip,nw_src=10.0.1.2,actions=output:$MiddleboxPort
+ovs-ofctl add-flow s2 priority=500,in_port=1,ip,nw_dst=10.0.1.2,actions=normal
+
+
+
+
+ovs-ofctl add-flow Camera3AP actions=normal
+ovs-ofctl add-flow Camera4AP actions=normal
--- Topology_Slicing.py
+++ Topology_Slicing.py
@@ -25,17 +25,18 @@ def CameraTopology():
          #High bandwidth#Camera topology
         info("Creating Nodes\n")
         #define cameras
-        Cameras1 = net.addStation('Cameras1', mac='00:00:00:00:00:01', position='1,0,0')
-        Cameras2 = net.addStation('Cameras2', mac='00:00:00:00:00:02', position='1000,0,0')
-        Cameras3 = net.addStation('Cameras3', mac='00:00:00:00:00:03', position='0,1000,0')
-        Cameras4 = net.addStation('Cameras4', mac='00:00:00:00:00:04', position='1000,1000,0')
+        Cameras1 = net.addStation('Cameras1', ip='10.0.0.1',mac='00:00:00:00:00:01', position='1,0,0')
+        Cameras2 = net.addStation('Cameras2', ip='10.0.0.2', mac='00:00:00:00:00:02', position='1000,0,0')
+        Cameras3 = net.addStation('Cameras3',ip='10.0.1.1', mac='00:00:00:00:00:03', position='0,1000,0')
+        Cameras4 = net.addStation('Cameras4',ip='10.0.1.2', mac='00:00:00:00:00:04', position='1000,1000,0')
         ###Use channel 1.  They do not overlap.
         CameraAccessPoint1=net.addAccessPoint('Camera1AP', ssid='ssid-Camera1AP', channel='1', position='1,5,0')
         CameraAccessPoint2=net.addAccessPoint('Camera2AP', ssid='ssid-Camera2AP', channel='1', position='1010,5,0')
-        CameraAccessPoint3=net.addAccessPoint('Camera3AP', ssid='ssid-Camera3AP', channel='1', position='10,5,0')
+        CameraAccessPoint3=net.addAccessPoint('Camera3AP', ssid='ssid-Camera3AP', channel='1', position='10,1000,0')
         CameraAccessPoint4=net.addAccessPoint('Camera4AP', ssid='ssid-Camera4AP', channel='1', position='1010,1005,0')
         #call remote so SDN controller works.  This example uses a dumb L2 switch for establishing a baseline.
         c1=net.addController('c1',controller=RemoteController,ip=ryu_ip,port=ryu_port)
+        #c1=net.addController('c1')
 
         s1=net.addSwitch('s1')
         s2=net.addSwitch('s2')
@@ -50,10 +51,10 @@ def CameraTopology():
         EmergencyAP3=net.addAccessPoint('Emergency3AP', ssid='ssid-Emergency3AP', channel='2', position='0,500,0')
         EmergencyAP4=net.addAccessPoint('Emergency4AP', ssid='ssid-Emergency4AP', channel='2', position='500,1000,0')
         #Add wire hosts
-        EmergencyCenter=net.addHost('Emerctr') #name is character limited
-        Middlebox1=net.addHost('Mid1')
-        Middlebox2=net.addHost('Mid2')
-        Datacenter=net.addHost('data')
+        EmergencyCenter=net.addHost('Emerctr',ip='10.0.5.0') #name is character limited
+        Middlebox1=net.addHost('Mid1',ip='10.0.4.1')
+        Middlebox2=net.addHost('Mid2',ip='10.0.4.2')
+        Datacenter=net.addHost('data',ip='10.0.6.1')
         #net.setPropagationModel(model='logDistance', exp=3)
 
         net.configureNodes()
@@ -64,10 +65,10 @@ def CameraTopology():
         net.addLink(Middlebox1,s1,port1=1,port2=1,bw=50)
         net.addLink(Middlebox2,s2,port1=1,port2=1,bw=50)
 
-        net.addLink(CameraAccessPoint1,s1,bw=20) #Connect to S1.  Connects to box 1
-        net.addLink(CameraAccessPoint2,s1,bw=20) #Connect to S1.  Connects to box 1
-        net.addLink(CameraAccessPoint3,s2,bw=20) #Connect to S2.  Connects to box 2
-        net.addLink(CameraAccessPoint4,s2,bw=20) #Connect to S2.  Connects to box 2
+        net.addLink(CameraAccessPoint1,s1,port1=2,bw=20) #Connect to S1.  Connects to box 1
+        net.addLink(CameraAccessPoint2,s1,port1=2,bw=20) #Connect to S1.  Connects to box 1
+        net.addLink(CameraAccessPoint3,s2,port1=2,bw=20) #Connect to S2.  Connects to box 2
+        net.addLink(CameraAccessPoint4,s2,port1=2,bw=20) #Connect to S2.  Connects to box 2
 
         net.addLink(EmergencyCenter,s5,port2=4,bw=100) #High bandwidth
         net.addLink(s1,s5,port2=1,bw=50)
@@ -79,10 +80,7 @@ def CameraTopology():
 
         net.addLink(Datacenter,s4,port2=2,bw=100) #Data storage platform.
 
-        net.addLink(Cameras1,CameraAccessPoint1,bw=10)
-        net.addLink(Cameras2,CameraAccessPoint2,bw=10)
-        net.addLink(Cameras3,CameraAccessPoint3,bw=10)
-        net.addLink(Cameras4,CameraAccessPoint4,bw=10)
+
 
 
         net.plotGraph(max_x=1050,max_y=1050)
