37336add8b34543716c72516efbde51ce9546cf7
timothy marchant <tim.marchant@outlook.com>
2026-04-26 15:15:29 -0400

    Added XtermCMDs.sh

---
+++ XtermCMDs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#open up terminal windows for each node
+xterm Mid1
+xterm Mid2
+xterm Cameras1
+xterm Cameras2
+xterm Cameras3
+xterm Cameras4
+xterm Emerctr
+xterm data
+xterm EmerCar1
